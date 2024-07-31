f3c9a448be2888bf14b563d973be297050116c07
Hritesh <hsworkmail.1@gmail.com>
2024-07-31 14:42:28 +0530

    added features (1: copy text, 2: remove extra spaces}

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,44 +2,60 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
+    // Fucntion to toggle text to upper case or lower case
     const handleUpClick = () => {
         let newText = text.toUpperCase()
         setText(newText)
     }
-    
-    const handleDwClick = ()=> {
+
+    const handleDwClick = () => {
         let newText = text.toLowerCase()
         setText(newText)
     }
 
+    // Function to extract emails from the text
     const emailExtracter = () => {
         let textArr = text.split(" ")
         let allEmails = ""
-        textArr.forEach((email)=> {
-            if (email.includes("@") && email.includes(".com")){
+        textArr.forEach((email) => {
+            if (email.includes("@") && email.includes(".com")) {
                 allEmails = allEmails + email + ",  "
             }
         })
         setEmail(allEmails)
     }
 
+    // Function to clear all texts
     const clearText = () => {
         let newText = ""
         setText(newText)
         setEmail(newText)
     }
 
-    const handleClick = ()=> {
-        if(toggle){
+    // Function to toggle between two states
+    const handleClick = () => {
+        if (toggle) {
             handleUpClick()
-        }else {
+        } else {
             handleDwClick()
         }
         setToggle(!toggle) // changing the toggle every time it is clicked
     }
-    
 
-    const handleOnChange = (event)=> {
+    // function to copy all the text in the text area
+    const handleCopy = () => {
+        let textElement = document.getElementById("myBox")
+        textElement.select()
+        navigator.clipboard.writeText(textElement.value)
+
+    }
+
+    const handleExtraSpace = ()=> {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
+    // this is to handle any change in our text area, without this we can't type in out text area.
+    const handleOnChange = (event) => {
         setText(event.target.value)
     }
 
@@ -47,7 +63,7 @@ export default function TextForm(props) {
     const [text, setText] = useState("")
     const [toggle, setToggle] = useState(true)
     const [email, setEmail] = useState("")
-    
+
     return (
         <>
             <div className="container my-3">
@@ -55,8 +71,10 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={handleClick}>Convirt to Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={clearText}>Clear all</button>
+                <button className="btn btn-primary" onClick={handleClick}>Toggle text case</button>
+                <button className="btn btn-primary mx-2" onClick={clearText}>Clear all</button>
+                <button className="btn btn-primary" onClick={handleCopy}>Copy text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove extra spaces</button>
                 <button className="btn btn-primary" onClick={emailExtracter}>Extract e-mails</button>
             </div>
 
@@ -71,7 +89,7 @@ export default function TextForm(props) {
                 <p>{email}</p>
             </div>
         </>
-        
+
 
     )
 }
